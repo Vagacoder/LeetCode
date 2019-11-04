43b9648914f783b65df7b9c07bdf6f00f5fbb963
Vagacoder <qiruihu@gmail.com>
2019-11-03 22:41:47 -0800

    update Q13

--- Python/Q0013-Roman_to_Integer.py
+++ Python/Q0013-Roman_to_Integer.py
@@ -17,7 +17,7 @@
 # as XXVII, which is XX + V + II.
 
 # Roman numeraks are usually written largest to smallest from left to right. However,
-# the numeral for four is not IIII. Instead, the number four is wirtten as IV. 
+# the numeral for four is not IIII. Instead, the number four is wirtten as IV.
 # Because the one is before the five we subtract it making four. The same principle
 # applies to the number nine, which is wirtten as IX, There are six instances where
 # subtraction is used:
@@ -50,42 +50,82 @@
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 # Solution #1
+
+
 class Solution1:
     def romanToInt(self, s: str) -> int:
-      length = len(s)
-      result = 0
+        length = len(s)
+        result = 0
 
-      for i in range(0, length):
-        if (s[i] == 'I'):
-          if(i<length-1 and (s[i+1] == "V" or s[i+1] == "X")):
-            result -= 1
-          else:
-            result += 1
-        elif (s[i] == 'V'):
-          result += 5
-        elif (s[i] == 'X'):
-          if(i<length-1 and (s[i+1] == "L" or s[i+1] == "C")):
-            result -= 10
-          else:
-            result += 10
-        elif (s[i] == 'L'):
-          result += 50
-        elif (s[i] == 'C'):
-          if(i<length-1 and (s[i+1] == "D" or s[i+1] == "M")):
-            result -= 100
-          else:
-            result += 100
-        elif (s[i] == 'D'):
-          result += 500
-        elif (s[i] == 'M'):
-          result += 1000
+        for i in range(0, length):
+            if (s[i] == 'I'):
+                if(i < length-1 and (s[i+1] == "V" or s[i+1] == "X")):
+                    result -= 1
+                else:
+                    result += 1
+            elif (s[i] == 'V'):
+                result += 5
+            elif (s[i] == 'X'):
+                if(i < length-1 and (s[i+1] == "L" or s[i+1] == "C")):
+                    result -= 10
+                else:
+                    result += 10
+            elif (s[i] == 'L'):
+                result += 50
+            elif (s[i] == 'C'):
+                if(i < length-1 and (s[i+1] == "D" or s[i+1] == "M")):
+                    result -= 100
+                else:
+                    result += 100
+            elif (s[i] == 'D'):
+                result += 500
+            elif (s[i] == 'M'):
+                result += 1000
 
-      return result
+        return result
 
-sol = Solution1()
+# Solution 2
+
+
+class Solution2:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        if ("IV" in s):
+            result -= 2
+        if ("IX" in s):
+            result -= 2
+        if ("XL" in s):
+            result -= 20
+        if ("XC" in s):
+            result -= 20
+        if ("CD" in s):
+            result -= 200
+        if ("CM" in s):
+            result -= 200
+
+        for character in s:
+            if (character == 'M'):
+                result += 1000
+            if (character == 'D'):
+                result += 500
+            if (character == 'C'):
+                result += 100
+            if (character == 'L'):
+                result += 50
+            if (character == 'X'):
+                result += 10
+            if (character == 'V'):
+                result += 5
+            if (character == 'I'):
+                result += 1
+
+        return result
+
+
+sol = Solution2()
 print("Input is III, expect output is 3, mine is: ", sol.romanToInt('III'))
 print("Input is IV, expect output is 4, mine is: ", sol.romanToInt('IV'))
 print("Input is IX, expect output is 9, mine is: ", sol.romanToInt('IX'))
 print("Input is LVIII, expect output is 58, mine is: ", sol.romanToInt('LVIII'))
-print("Input is MCMXCIV, expect output is 1994, mine is: ", sol.romanToInt('MCMXCIV'))
-
+print("Input is MCMXCIV, expect output is 1994, mine is: ",
+      sol.romanToInt('MCMXCIV'))
