d4cd85b6be1e0ac1625432cac58168ae133a801d
Vagacoder <qiruihu@gmail.com>
2019-11-04 22:41:29 -0800

    update Q7 and Q9

--- Python/Q0007_Reverse_Integer.py
+++ Python/Q0007_Reverse_Integer.py
@@ -19,33 +19,53 @@
 # the 32-bit singed integer range: [-2^31, 2^31-1]. For the purpose of this problem,
 # assume that your function returns 0 when the reversed integer overflows
 
-# Solution #1
+# Solution #1 (convert to string)
+
+
 class Solution1:
     def reverse(self, x: int) -> int:
-      if (x < 0):
-        isNegative:bool = True
-        x = -x
-      else:
-        isNegative:bool = False
-      
-      xString: str = "" + str(x)
-      resultString: str = ""
+        if (x < 0):
+            isNegative: bool = True
+            x = -x
+        else:
+            isNegative: bool = False
 
-      for letter in xString:
-        resultString = letter + resultString
+        xString: str = "" + str(x)
+        resultString: str = ""
 
-      result: int = int(resultString)
+        for letter in xString:
+            resultString = letter + resultString
 
-      if (isNegative):
-        result = -result
+        result: int = int(resultString)
 
-      if (result > (2**31 -1) or result < (-2**31)):
+        if (isNegative):
+            result = -result
+
+        if (result > (2**31 - 1) or result < (-2**31)):
+            result = 0
+
+        return result
+
+
+# Solution 2
+class Solution2:
+    def reverse(self, x: int) -> int:
         result = 0
-        
-      return result
+        while (x != 0):
+            if (x >= 0 or (x < 0 and x % 10 == 0)):
+                result = result * 10 + x % 10
+                x = x // 10
+            else:
+                result = result * 10 + x % 10 - 10
+                x = x // 10 + 1
+
+        if (result > (2**31 - 1) or result < (-2**31)):
+            result = 0
+
+        return result
 
 
-sol = Solution1()
+sol = Solution2()
 print("Input 120, output should be 21, mine is: ", sol.reverse(120))
 print("Input 123, output should be 321, mine is: ", sol.reverse(123))
 print("Input -120, output should be -21, mine is: ", sol.reverse(-120))
--- Python/Q0009-Palindrome_Number.py
+++ Python/Q0009-Palindrome_Number.py
@@ -12,7 +12,7 @@
 # Input: -121
 # Output: false
 # Explaination: From left to right, it reads -121; From right to left, it reads
-# 121-, not same 
+# 121-, not same
 
 # Example 3:
 # Input: 10
@@ -22,24 +22,43 @@
 # Coud you solve it without converting the integer to a string?
 
 # Solution 1 (to string)
+
+
 class Solution1:
     def isPalindrome(self, x: int) -> bool:
-        xString : str = str(x)
+        xString: str = str(x)
         return self.isPalindromeString(xString)
 
     def isPalindromeString(self, xString: str) -> bool:
         if (len(xString) == 1):
             return True
         elif (len(xString) == 2):
-            return (xString[0] == xString[1]) 
+            return (xString[0] == xString[1])
         else:
             return ((xString[0] == xString[-1]) and self.isPalindromeString((xString[1: -1])))
 
-sol = Solution1()
+# Solution 2 (reverse half of the number)
+
+
+class Solution2:
+    def isPalindrome(self, x: int) -> bool:
+        if(x < 0 or (x % 10 == 0 and x != 0)):
+            return False
+
+        revertedNum = 0
+        while (x > revertedNum):
+            revertedNum = revertedNum * 10 + x % 10
+            x = x//10
+
+        return x == revertedNum or x == revertedNum//10
+
+
+sol = Solution2()
 print("Input is 121, output should be True, mine is: ", sol.isPalindrome(121))
 print("Input is 1121, output should be False, mine is: ", sol.isPalindrome(-121))
 print("Input is 10, output should be False, mine is: ", sol.isPalindrome(10))
 print("Input is 1, output should be True, mine is: ", sol.isPalindrome(1))
 print("Input is 11, output should be True, mine is: ", sol.isPalindrome(11))
 print("Input is 1021, output should be False, mine is: ", sol.isPalindrome(1021))
-print("Input is 1000021, output should be False, mine is: ", sol.isPalindrome(1000021))
+print("Input is 1000021, output should be False, mine is: ",
+      sol.isPalindrome(1000021))
