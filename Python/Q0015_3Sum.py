c8e505fb6852dc2115220f3a517896c5de2b7b83
Vagacoder <qiruihu@gmail.com>
2019-12-28 21:57:23 -0800

    working at Q15

--- Python/Q0015_3Sum.py
+++ Python/Q0015_3Sum.py
@@ -15,9 +15,7 @@
 
 from typing import List
 
-# Solution 1 brutal force O(N^3)
-
-
+# Solution 1 brutal force O(N^3) exceed time limit
 class Solution1:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         N: int = len(nums)
@@ -33,9 +31,7 @@ class Solution1:
 
         return result
 
-# Solution 2 faster
-
-
+# Solution 2 faster but still exceed time limit
 class Solution2:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -52,19 +48,42 @@ class Solution2:
                     low += 1
                 else:
                     temp = [nums[i], nums[low], nums[high]]
+                    # temp.sort()
+                    if not temp in result:
+                        result.append(temp)
+                    high -= 1
+                    low += 1
+
+        return result
+
+# Solution 3 still exceed time limit
+class Solution3:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        numPool = {}
+        if len(nums) > 0:
+            numPool[nums[0]] = 0
+        N: int = len(nums)
+        result = []
+        for i in range(N):
+            for j in range(i+1, N):
+                sumOfIandJ = nums[i] + nums[j]
+                if (-sumOfIandJ in numPool) and (numPool)[-sumOfIandJ] != i and (numPool)[-sumOfIandJ] != j:
+                    temp = [nums[i], nums[j], -(nums[i] + nums[j])]
                     temp.sort()
                     if not temp in result:
                         result.append(temp)
+                else:
+                    numPool[nums[j]] = j
 
         return result
 
 
-sol = Solution2()
+sol = Solution3()
 a = [-1, 0, 1, 2, -1, -4]
-# b = []
-# c = [0]
-# d = [1]
+b = []
+c = [0]
+d = [1]
 print(sol.threeSum(a))
-# print(sol.threeSum(b))
-# print(sol.threeSum(c))
-# print(sol.threeSum(d))
+print(sol.threeSum(b))
+print(sol.threeSum(c))
+print(sol.threeSum(d))
