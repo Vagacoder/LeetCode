86c4ca1eb85df58e58d93f7d860856cb041aac6c
Vagacoder <qiruihu@gmail.com>
2020-10-25 10:27:41 -0700

    10/25 Q322 is done

--- Python/DynamicProgram/Q0322_CoinChange.py
+++ Python/DynamicProgram/Q0322_CoinChange.py
@@ -45,6 +45,7 @@
 class Solution:
 
     # * Solution 1
+    # ! Brute force
     def coinChange1(self, coins: list, amount: int) -> int:
         
         def dp(n:int):
@@ -65,6 +66,51 @@ class Solution:
 
         return dp(amount)
         
+
+    # * Solution 2
+    # ! with a memo
+    def coinChange2(self, coins: list, amount: int) -> int:
+        memo = dict()
+
+        def dp(n:int):
+            if n in memo: 
+                return memo[n]
+            
+            if n == 0:
+                return 0
+            if n < 0:
+                return -1
+
+            result = float('inf')
+
+            for coin in coins:
+                subResult = dp(n -coin)
+                if subResult < 0:
+                    continue
+                result = min(result, 1 + subResult)
+            
+            memo[n] = result if result != float('inf') else -1
+
+            return memo[n]
+
+        return dp(amount)
+
+
+    # * Solution 3
+    # ! from bottom up
+    def coinChange3(self, coins: list, amount: int) -> int:
+        dp = [amount+1]*(amount+1)
+        dp[0] = 0
+
+        for i in range(amount+1):
+            for coin in coins:
+                if i - coin < 0:
+                    continue
+                dp[i] = min(dp[i], 1 + dp[i-coin])
+
+        return -1 if dp[amount] == amount + 1 else dp[amount]
+        
+
 
 sol = Solution()
 a1 = [1, 2, 5]
