9e10935e51112047b515896d2911908d2ea63361
Vagacoder <qiruihu@gmail.com>
2020-11-23 11:58:09 -0800

    11/23 Q213 is done

--- Python/DynamicProgram/Q0213_HouseRobber2.py
+++ Python/DynamicProgram/Q0213_HouseRobber2.py
@@ -38,3 +38,39 @@
 #     1 <= nums.length <= 100
 #     0 <= nums[i] <= 1000
 
+#%%
+
+class Solution:
+
+    # * Solution 1
+    def rob1(self, nums: list) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+
+        def robRange(start:int, end:int)->int:
+            dp = [0] * n
+            dp[start] = nums[start]
+            dp[start+1] = max(nums[start], nums[start+1])
+            for i in range(start+2,end+1):
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+            return dp[end]
+
+
+        return max(robRange(0, n-2), robRange(1, n-1))
+
+
+
+sol = Solution()
+nums = [2,3,2]
+r1 = sol.rob1(nums)
+print(r1)
+
+
+# %%
